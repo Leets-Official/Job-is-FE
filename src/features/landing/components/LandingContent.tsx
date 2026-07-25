5a6cff033f023b2a0079a1ae04ac0759eb0cf17a
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-25 15:28:39 +0900

    feat: 랜딩 페이지에서 로그인 페이지로 이동하는 동작 연결

--- src/features/landing/components/LandingContent.tsx
+++ src/features/landing/components/LandingContent.tsx
@@ -2,12 +2,19 @@ import type { LandingOutletContext } from '@/components/layout/LandingLayout';
 import LandingFeatureGrid from '@/features/landing/components/LandingFeatureGrid';
 import LandingHero from '@/features/landing/components/LandingHero';
 
+interface LandingContentProps extends LandingOutletContext {
+  onStart: () => void;
+  onLogin: () => void;
+}
+
 export default function LandingContent({
   headline,
   typedHeadline,
   isIntroComplete,
   shouldPlayRevisitAnimation,
-}: LandingOutletContext) {
+  onStart,
+  onLogin,
+}: LandingContentProps) {
   return (
     <>
       <LandingHero
@@ -15,6 +22,8 @@ export default function LandingContent({
         typedHeadline={typedHeadline}
         isIntroComplete={isIntroComplete}
         shouldPlayRevisitAnimation={shouldPlayRevisitAnimation}
+        onStart={onStart}
+        onLogin={onLogin}
       />
       <LandingFeatureGrid isIntroComplete={isIntroComplete} />
     </>
