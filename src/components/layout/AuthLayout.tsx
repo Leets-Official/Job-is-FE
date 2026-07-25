55550c530b9a499cecf9e861d400d4450c61e8da
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-25 23:52:57 +0900

    fix: 인증 화면 가로 스크롤 방지

--- src/components/layout/AuthLayout.tsx
+++ src/components/layout/AuthLayout.tsx
@@ -4,7 +4,7 @@ export default function AuthLayout() {
   const location = useLocation();
 
   return (
-    <div className="flex min-h-screen flex-col">
+    <div className="flex min-h-screen flex-col overflow-x-clip">
       <div key={location.pathname} className="page-content-enter flex min-h-0 flex-1 flex-col">
         <Outlet />
       </div>
