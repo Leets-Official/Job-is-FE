34a527f6d8e95b174ccdfa27a8cee14ac1363817
Lee KyeongJoon <rudwns0224@naver.com>
2026-07-25 15:26:18 +0900

    refactor: RecommendationPendingPage를 RecommendationsPage로 이름 변경

---
+++ src/pages/RecommendationsPage/RecommendationsPage.tsx
@@ -0,0 +1,5 @@
+import RecommendationPendingContent from '@/features/jobs/components/RecommendationPendingContent';
+
+export default function RecommendationsPage() {
+  return <RecommendationPendingContent />;
+}
